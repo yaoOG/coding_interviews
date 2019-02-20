763a96f21a4d7248e0de674f5b1fffe7b06c1dcf
zhuyao <415170029@qq.com>
2019-02-20 21:49:31 +0800

    addition：topic38,topic39,topic40

--- src/topic/Topic38.java
+++ src/topic/Topic38.java
@@ -1,56 +1,52 @@
 package topic;
 
+import java.util.Scanner;
+
 /**
  * @author zhuyao
  * @date 2019/02/10
  */
 public class Topic38 {
     /**
-     * 题目：输入一个字符串，打印出该字符事中字符的所有排列。例如输入字符串abc。
-     * 则打印出由字符a、b、c 所能排列出来的所有字符串abc、acb、bac、bca、cab和cba。
-     *
-     * @param chars 待排序的字符数组
+     * 题目：输入一个字符串，打印出该字符事中字符的所有排列。
+     * 例如输入字符串abc，则打印出由字符a、b、c 所能排列出来的所有字符串abc、acb、bac、bca、cab和cba。
      */
-    public static void permutation(char[] chars) {
-        // 输入较验
-        if (chars == null || chars.length < 1) {
-            return;
-        }
-        // 进行排列操作
-        permutation(chars, 0);
+
+    public static void main(String[] args) {
+        String str = "";
+
+        Scanner scan = new Scanner(System.in);
+
+        str = scan.nextLine();
+
+        permutation(str.toCharArray(), 0);
     }
 
     /**
      * 求字符数组的排列
      *
-     * @param chars 待排列的字符串
+     * @param str   待排列的字符串
      * @param begin 当前处理的位置
      */
-    public static void permutation(char[] chars, int begin) {
+    public static void permutation(char[] str, int begin) {
+        if (begin >= str.length)
+            return;
         // 如果是最后一个元素了，就输出排列结果
-        if (chars.length - 1 == begin) {
-            System.out.print(new String(chars) + " ");
+        if (begin == str.length - 1) {
+            System.out.println(String.valueOf(str));
         } else {
-            char tmp;
             // 对当前还未处理的字符串进行处理，每个字符都可以作为当前处理位置的元素
-            for (int i = begin; i < chars.length; i++) {
-                // 下面是交换元素的位置
-                tmp = chars[begin];
-                chars[begin] = chars[i];
-                chars[i] = tmp;
-
+            for (int j = begin; j < str.length; j++) {
+                char temp = str[j];
+                str[j] = str[begin];
+                str[begin] = temp;
                 // 处理下一个位置
-                permutation(chars, begin + 1);
+                permutation(str, begin + 1);
+
+                temp = str[j];
+                str[j] = str[begin];
+                str[begin] = temp;
             }
         }
-    }
-
-    public static void main(String[] args) {
-        char[] c1 = {'a', 'b', 'c'};
-        permutation(c1);
-        System.out.println();
-
-//        char[] c2 = {'a', 'b', 'c', 'd'};
-//        permutation(c2);
     }
 }
